a5a7e1adf5ffd4da842a5d03907520fc4776fa61
datduyng <datnguyen5653@gmail.com>
2024-03-23 20:21:27 -0700

    update

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -21,11 +21,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         }}
       /> */}
       <CopilotSidebar
-        defaultOpen={true}
+        defaultOpen={false}
         labels={{
           title: 'BrainBite - Your Personal Learning Assistant',
           // title: "You are a book recommender expert. Helping student learn things faster. You only know about books and help students to find the right book for them. You can also help them to understand the book better.",
-          initial: "Hi! 👋 How can I assist you today?",
+          initial: "Hi! 👋 Ask me any question about learning material on the left",
         }}
       >
         <Component {...pageProps} />
