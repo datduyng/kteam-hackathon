7227dcec2da75f819e0c866721cb2307d685db45
datduyng <datnguyen5653@gmail.com>
2024-03-23 19:58:51 -0700

    add side bar

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -2,7 +2,7 @@ import { Toaster } from '@/components/ui/toaster'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { CopilotKit } from "@copilotkit/react-core";
-import { CopilotSidebar } from "@copilotkit/react-ui";
+import { CopilotPopup, CopilotSidebar } from "@copilotkit/react-ui";
 import "@copilotkit/react-textarea/styles.css";
 import "@copilotkit/react-ui/styles.css"
 import { useMakeCopilotReadable } from "@copilotkit/react-core";
@@ -11,10 +11,19 @@ function MyApp({ Component, pageProps }: AppProps) {
   useMakeCopilotReadable("You are helping Dominique to learn faster.");
   return <>
     <CopilotKit url="/api/copilotkit/openai">
+      {/* We can have either sidebar or popup. We can't have both at the same time. */}
+      {/* <CopilotPopup
+        defaultOpen={true}
+        shortcut='Command-K'
+        labels={{
+          title: "You are a book recommender expert. Helping student learn things faster. You only know about books and help students to find the right book for them. You can also help them to understand the book better.",
+          initial: "Hi! 👋 How can I assist you today?",
+        }}
+      /> */}
       <CopilotSidebar
         defaultOpen={true}
         labels={{
-          title: "You are a book recommender expert. Helping student learn things faster. You only know about books and help students to find the right book for them. You can also help them to understand the book better.",
+          // title: "You are a book recommender expert. Helping student learn things faster. You only know about books and help students to find the right book for them. You can also help them to understand the book better.",
           initial: "Hi! 👋 How can I assist you today?",
         }}
       >
