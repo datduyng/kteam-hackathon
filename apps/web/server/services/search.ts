7f58c55a475435892c464d02a990b8039edd2b2f
datduyng <datnguyen5653@gmail.com>
2024-11-15 13:50:27 -0800

    Add web search

--- apps/web/server/services/search.ts
+++ apps/web/server/services/search.ts
@@ -13,6 +13,14 @@ export type SearchImageResponse = {
   }>;
 }
 
+export type WebSearchResponse = {
+  orginic_results: Array<{
+    link: string;
+    name: string;
+    snippet: string;
+  }>;
+}
+
 export const imageSearch = async (query: string) => {
   return fetch('https://openai-gateway.vercel.app/api/websearch/image?query=' + query, {
     headers: {
@@ -27,6 +35,27 @@ export const imageSearch = async (query: string) => {
       }
 
       return data.value || [];
+    })
+    .catch((e: any) => {
+      console.error('Failed to fetch image search results', e);
+      return [];
+    });
+}
+
+export const webSearch = async (query: string) => {
+  return fetch('https://openai-gateway.vercel.app/api/websearch?query=' + query, {
+    headers: {
+      'x-api-key': process.env.GATEWAY_SERVER_SECRET_KEY || '',
+    }
+  })
+    .then(res => res.json())
+    .then((data: WebSearchResponse) => {
+      if ((data as any)?.error) {
+        console.error('Failed to fetch web search', data);
+        return [];
+      }
+
+      return data.orginic_results || [];
     })
     .catch((e: any) => {
       console.error('Failed to fetch image search results', e);
